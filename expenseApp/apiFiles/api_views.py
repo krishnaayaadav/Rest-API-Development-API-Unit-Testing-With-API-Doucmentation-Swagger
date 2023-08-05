e858a3bb99630d6061febfea4145a89ab833cc37
Krishna Yadav <krishnadj7431@gmail.com>
2023-08-05 14:21:48 +0530

    api response is customized

--- expenseApp/apiFiles/api_views.py
+++ expenseApp/apiFiles/api_views.py
@@ -13,10 +13,13 @@ bad_request  = status.HTTP_400_BAD_REQUEST
 class ExpenseAPIView(APIView):
     # get all expense here
     def get(self, request, format=None):
-
         all_exp = Expense.objects.all()
         serializer = exp_serializer.ExpenseSerializer(all_exp, many=True)
-        return Response(serializer.data, status=ok_restponse)
+        response = {
+            'msg': "All expense data",
+            'data': serializer.data,
+        }
+        return Response(response, status=ok_restponse)
     
     # add/post/insert new expense into db
     def post(self, request, format=None):
@@ -24,7 +27,11 @@ class ExpenseAPIView(APIView):
         serializer  = exp_serializer.ExpenseSerializer(data=request.data)
         if serializer.is_valid():  # checking serializer validation here
             serializer.save()      # saving data into db
-            return Response(serializer.data, status=created)
+            response = {
+                'msg': 'Congrats! expense successfully inserted',
+                'data': serializer.data
+            }
+            return Response(response, status=created)
         else:
             return Response(serializer.errors, status=bad_request)
         
@@ -48,6 +55,35 @@ class ExpenseUpdateAPI(APIView):
 
         else: # if expense found
             serializer = exp_serializer.ExpenseSerializer(expense)
-            return Response(serializer.data, status=ok_restponse)
+            response = {
+                'msg': f"Detail of expense of id: {expId}",
+                "data": serializer.data
+            }
+            return Response(response, status=ok_restponse)
     
-    
+    # patch  request for partial updation
+    def patch(self, request, expId, format=None):
+        error_response = {
+            'data': f"Expense does found with this {expId}",
+            'status': not_found
+        }
+        try:
+            expense = Expense.objects.get(pk=expId)
+
+        except Expense.DoesNotExist:
+            return Response(error_response, status=not_found)  # expense does not exist error
+
+        except Expense.MultipleObjectsReturned:
+            return Response(error_response, status=not_found)  # multiple expense found errro
+
+
+        else: # if expense found
+            serializer = exp_serializer.ExpenseSerializer(expense, data=request.data, partial=True, context={'expense': expense})
+
+            # checking serializer validation here
+            if serializer.is_valid():
+                serializer.save() # then store serialized data into db
+                return Response(serializer.data, ok_restponse)
+            else:
+                return Response(serializer.errors, status=bad_request)
+        
--- expenseApp/apiFiles/serializers.py
+++ expenseApp/apiFiles/serializers.py
@@ -4,12 +4,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class ExpenseSerializer(serializers.Serializer):
-    exp_date    = serializers.DateField(required=True, error_messages={'required': "Expense date is required"})
-    exp_title   = serializers.CharField(required=True, error_messages={'required': 'Expense title is required'})
+    exp_date    = serializers.DateField(required=False, error_messages={'required': "Expense date is required"})
+    exp_title   = serializers.CharField(required=False, error_messages={'required': 'Expense title is required'})
     exp_description = serializers.CharField(style={
         'base_template': "textarea.html"
-    }, required=True, error_messages={'required': 'Expense description is required. Explain what, when, where expense happed'})
-    exp_user        = serializers.CharField(required=True, error_messages={'required': 'Expense user is required'})
+    }, required=False, error_messages={'required': 'Expense description is required. Explain what, when, where expense happed'})
+    exp_user        = serializers.CharField(required=False, error_messages={'required': 'Expense user is required'})
 
     # custom serializer validation
     def validate(self, attrs):
@@ -17,22 +17,40 @@ class ExpenseSerializer(serializers.Serializer):
         exp_title= attrs.get('exp_title', None)
         exp_desc = attrs.get('exp_description', None)
         exp_user = attrs.get('exp_user', None)
-        
+
+        expense = self.context.get('expense', None) # for partial updation
+
+        # all field required validation here
+        if not expense:
+
+            field_name = ('title', 'description', 'user', 'date',)
+            data       = (exp_title, exp_desc,  exp_user, exp_date)
+
+            for i in range(0,4):
+                field_data = data[i] # fiedl data from user
+                field_naam = field_name[i]
+
+                if not field_data:
+                    raise serializers.ValidationError({f'{field_naam}': f'Expense {field_naam} is required'})
+                
         # title validation
-        if len(exp_title) < 7:
-            raise serializers.ValidationError({'exp_title': 'Expense title at least contains 7 characters'})
+        if exp_title:
+            if len(exp_title) < 7:
+                raise serializers.ValidationError({'exp_title': 'Expense title at least contains 7 characters'})
         
         # description validation
-        if str(len(exp_desc)) <11:
-            raise serializers.ValidationError({'exp_description': "Expense description atleast contains 11 characters"})
-        
+        if exp_desc:
+            if len(exp_desc) <11:
+                raise serializers.ValidationError({'exp_description': "Expense description atleast contains 11 characters"})
+
         # checking here valid user
-        try:
-            user = User.objects.get(username=exp_user)
-        except:
-            raise serializers.ValidationError({'exp_user': "Expense user is invalid"})
-        else:
-            attrs['exp_user'] = user # changing via user object here
+        if exp_user:
+            try:
+                user = User.objects.get(username=exp_user)
+            except:
+                raise serializers.ValidationError({'exp_user': "Expense user is invalid"})
+            else:
+                attrs['exp_user'] = user # changing via user object here
 
         return attrs
    
@@ -40,3 +58,14 @@ class ExpenseSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Expense.objects.create(**validated_data)
     
+    def update(self, instance, validated_data):
+        # instance means old data
+        # validated_data: means new which is comming for updation
+        
+        # updating instance data here
+        instance.exp_title = validated_data.get('exp_title', instance.exp_title)
+        instance.exp_date  = validated_data.get('exp_date', instance.exp_date)
+        instance.exp_user  = validated_data.get('exp_user', instance.exp_user)
+        instance.exp_description = validated_data.get('exp_description', instance.exp_description)
+        instance.save() # saving data here
+        return instance
