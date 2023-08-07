0c8f6491dbf67e48682c32007d9d2ffeefe83288
krishna <krishnadj7431@gmail.com>
2023-08-07 11:50:07 +0530

    valid and invalid single api test is configured

--- expenseApp/tests.py
+++ expenseApp/tests.py
@@ -7,14 +7,14 @@ from expenseApp.apiFiles.serializers import ExpenseSerializer
 
 from django.urls import reverse
 
-# response classes 
+# Response classes
 ok_response = status.HTTP_200_OK
 bad_request = status.HTTP_400_BAD_REQUEST
 created     = status.HTTP_201_CREATED
 not_found   = status.HTTP_404_NOT_FOUND
 no_content  = status.HTTP_204_NO_CONTENT
 
-
+# API Root reverse here
 get_or_post_expenses = 'get_or_post_expense'
 update_expenses      = 'update_delete_post'
 # APIClient as some app assume
@@ -73,6 +73,93 @@ class ExpenseTestSetUp(TestCase):
          # print('\n Count test case passed')
          pass
 
-         
+# testing all expenses
+class TestAllExpenses(ExpenseTestSetUp):
+   
+   # testing all expense here
+   def test_get_all_expense(self):
+      response = client.get(
+         reverse(get_or_post_expenses)
+      )
+      # checking status code is same or not
+      try:
+         self.assertEqual(response.status_code, ok_response)
+      except:
+         pass
+
+      all_exps = Expense.objects.all()
+      serializer = ExpenseSerializer(all_exps, many=True)
+      res = {
+            'msg': "All expense data",
+            'data': serializer.data,
+         }
+      
+      # checking here reponse.data with expected data
+      try:
+         self.assertEqual(response.data, res)
+
+      except:
+         print('\n Response data not matched')
+
+# test single expense valid and invalid 
+class TestSingleExpense(ExpenseTestSetUp):
+   
+   # test valid expense
+   def test_valid_expense(self):
+
+      response = client.get(reverse(update_expenses, kwargs={'expId': 3}))
+
+      try:
+         exp = Expense.objects.get(pk = 3)
+      except:
+         exp = None
+
+      finally:
+         serializer = ExpenseSerializer(exp)
+         res = {
+               'msg': f"Detail of expense of id: {3}",
+               "data": serializer.data
+         }
+         try:
+            self.assertEqual(response.data, res)
+         except:
+            print('\n valid expense test failed')
+   
+   # test valid expense
+   def test_invalid_expense(self):
+      expId = 30
+
+      response = client.get(reverse(update_expenses, kwargs={'expId': expId}))
+
+      try:
+         exp = Expense.objects.get(pk = expId)
+      except:
+         exp = None
+
+      finally:
+         serializer = ExpenseSerializer(exp)
+         res = {
+               'msg': f"Detail of expense of id: {expId}",
+               "data": serializer.data
+         }
+         try:
+            self.assertNotEqual(response.data, res)
+            
+         except:
+            print('\n valid expense test failed')
+   
+# expense post here
+class TestPostExpenses(ExpenseTestSetUp):
+
+   def test_post_expense_data(self):
+
+      user = User.objects.get(username="krish")
+      title= "Demo expense title"
+      description = 'This is expense description here'
+      date        = '2021-02-10'
+      amount      = 500
+
+      client.get(reverse(get_or_post_expenses))
+
 
 
